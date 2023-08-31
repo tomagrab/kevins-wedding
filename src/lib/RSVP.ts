f0ea41ea5adea34790afaad601d89f6c7491a003
tomagrab <tommygrabowski@MBP-11262.lan>
2023-08-30 19:00:52 -0500

    Set up database via Prisma

---
+++ src/lib/RSVP.ts
@@ -0,0 +1,12 @@
+// Type for RSVP
+
+export type RSVP = {
+  id: string;
+  name: string;
+  email: string;
+  attending: boolean;
+  guests: number;
+  message: string;
+  createdAt: string;
+  updatedAt: string;
+};
